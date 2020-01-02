dec59bf27480f354e9566b48ce6b8235c546368c
djalde <djalde.gomes@academico.ifpb.edu.br>
2020-01-02 19:08:21 -0300

    er

---
+++ api/gerap2.php
@@ -0,0 +1,30 @@
+<?php 
+
+function gerap2() {
+$servicos = [];
+
+$hostname = shell_exec("hostname");
+$regexhost='/(.+)/';
+preg_match($regexhost,$hostname,$matches3);
+$servicos[0]["nome"] = "hostname";
+$servicos[0]["valor"] = $matches3[0];
+$regexmem = '/Mem:\s+\d+\s+(\d+)\s+(\d+)/';
+$regexdisk='/\/dev\/sda4\s+\w+\s+(\w+)\s+(\w+)/';
+$memoria = shell_exec("free");
+$disco = shell_exec("df -h");
+preg_match($regexmem,$memoria,$matches);
+preg_match($regexdisk,$disco,$matches2);
+$servicos[1]["nome"] = "ram";
+$servicos[1]["used"] = $matches[1];
+$servicos[1]["free"] = $matches[2];
+$servicos[2]["nome"] = "disk";
+$servicos[2]["used"] = $matches2[1];
+$servicos[2]["free"] = $matches2[2];
+
+return $servicos;
+}
+
+
+
+
+?>
---
+++ api/index.php
@@ -0,0 +1,10 @@
+<?php
+require('gerap2.php');
+
+$x = gerap2();
+
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+echo json_encode($x);
+?>
