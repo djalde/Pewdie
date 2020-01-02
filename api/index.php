b62f4683926cc273571a07b611d4309d9b8d547b
djalde <djalde.gomes@academico.ifpb.edu.br>
2020-01-02 20:53:44 -0300

    gerap2 ok

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
 require('gerap2.php');
 
-$x = gerap2();
+
+$nada = gerap2();
 
 header("Content-type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-
-echo json_encode($x);
+echo json_encode($nada);
 ?>
